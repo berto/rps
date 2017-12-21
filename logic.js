3c7f0527a3382fef921a1c5f19ae9999a0ac8ccd
Roberto Ortega <berto.ort@gmail.com>
2017-12-21 13:14:55 -0700

    Add computer logic for choosing and determining winner

--- logic.js
+++ logic.js
@@ -1,9 +1,33 @@
+const possibleMoves = ['✋', '✊', '✌️']
+const winnerMoves = {
+  paper: 'rock',
+  rock: 'scissor',
+  scissor: 'paper'
+}
+
 function computerMove() {
-  return 'rock'
+  const randomIndex = Math.floor(Math.random() * possibleMoves.length)
+  return possibleMoves[randomIndex]
 }
 
 function calculateWinner(userMove, aiMove) {
-  return '???'
+  aiMove = convertAiMove(aiMove)
+  if (userMove === aiMove) {
+    return 'It\' a draw!'
+  } else {
+    return winnerMoves[userMove] === aiMove ? "You Won!" : "You Lost!"
+  }
+}
+
+function convertAiMove(aiMove) {
+  switch(aiMove) {
+    case '✋':
+      return 'paper'
+    case '✊':
+      return 'rock'
+    default:
+      return 'scissor'
+  }
 }
 
 module.exports = {
